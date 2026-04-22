8a9234541b88099320b689dcdf363a9f249f686d
Adri Pando <babybread25@gmail.com>
2026-04-21 23:44:12 -0400

    arreglo de prenda.js

--- backend/src/models/Prenda.js
+++ backend/src/models/Prenda.js
@@ -1,15 +1,16 @@
 const pool = require('../config/database')
-const { findByUser } = require('./Outfit')
 
 const Prenda = {
   async create({ user_id, nombre, tipo, talla, color, temporada, marca, material, imagen_url }) {
-    const result = await pool.query()
-        `INSERT INTO prendas (user_id, nombre, tipo, talla, color, temporada, marca, material, imagen_url) 
-         VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) 
-         RETURNING *`, 
-         [user_id, nombre, tipo, talla, color, temporada, marca, material, imagen_url]
+    const result = await pool.query(
+      `INSERT INTO prendas (user_id, nombre, tipo, talla, color, temporada, marca, material, imagen_url) 
+       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) 
+       RETURNING *`,
+      [user_id, nombre, tipo, talla, color, temporada, marca, material, imagen_url]
+    )
     return result.rows[0]
   },
+
   async findByUser(user_id) {
     const result = await pool.query(
       `SELECT * FROM prendas WHERE user_id = $1 ORDER BY created_at DESC`,
@@ -17,25 +18,29 @@ const Prenda = {
     )
     return result.rows
   },
+
   async findById(id) {
     const result = await pool.query(
       `SELECT * FROM prendas WHERE id = $1`,
       [id]
     )
-    return result.rows[0] 
+    return result.rows[0]
   },
+
   async update(id, { nombre, tipo, talla, color, temporada, marca, material, imagen_url }) {
     const result = await pool.query(
       `UPDATE prendas 
-       SET nombre = $1, tipo = $2, talla = $3, color = $4, temporada = $5, marca = $6, material = $7, imagen_url = $8 
-       WHERE id = $9 
+       SET nombre=$1, tipo=$2, talla=$3, color=$4, temporada=$5, marca=$6, material=$7, imagen_url=$8 
+       WHERE id=$9 
        RETURNING *`,
       [nombre, tipo, talla, color, temporada, marca, material, imagen_url, id]
     )
     return result.rows[0]
   },
+
   async delete(id) {
     await pool.query('DELETE FROM prendas WHERE id = $1', [id])
   }
 }
+
 module.exports = Prenda
