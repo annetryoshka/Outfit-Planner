33fc963215c6bded2d201bd389edc538e664caca
Adri Pando <babybread25@gmail.com>
2026-09-04 02:13:58 -0400

    limit arreglado otra vez y homepage

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const cors = require('cors')
 const dotenv = require('dotenv')
 dotenv.config()
 
-
 const pool = require('./src/config/database')
 const authRoutes = require('./src/routes/auth')
 const authMiddleware = require('./src/middleware/auth')
@@ -17,27 +16,32 @@ const tryonRoutes = require('./src/routes/tryon')
 const guardadoRoutes = require('./src/routes/guardado')
 const likeRoutes = require('./src/routes/like')
 const followerRoutes = require('./src/routes/follower')
-const notificationRoutes = require('./src/routes/notification')  
+const notificationRoutes = require('./src/routes/notification')
 const { specs, swaggerUi } = require('./src/config/swagger')
 const passport = require('passport')
 const logger = require('./src/config/logger')
 
 const app = express()
 const PORT = process.env.PORT || 3000
+app.set('trust proxy', 1)
 
 app.use(cors({
   origin: [
-    'http://localhost:5173',                  
+    'http://localhost:5173',
     'https://outfit-planner-1-fu4w.onrender.com'
   ],
   credentials: true
-}));
+}))
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
+
 require('./src/config/passport')
 app.use(passport.initialize())
-app.use(generalLimiter) 
+
+app.use(generalLimiter)
+
+//rutas
 app.use('/api/auth', authRoutes)
 app.use('/api/prendas', prendaRoutes)
 app.use('/api/outfits', outfitRoutes)
@@ -51,6 +55,7 @@ app.use('/api/followers', followerRoutes)
 app.use('/api/notifications', notificationRoutes)
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(specs))
 
+//ruta raíz
 app.get('/', (req, res) => {
   res.json({ message: 'Outfit Planner API funcionando uwuwewe' })
 })
@@ -59,6 +64,7 @@ app.get('/api/perfil', authMiddleware, (req, res) => {
   res.json({ message: 'Ruta protegida', usuario: req.usuario })
 })
 
+//global de errores
 app.use((err, req, res, next) => {
   logger.error('Error en servidor:', {
     message: err.message,
@@ -71,7 +77,6 @@ app.use((err, req, res, next) => {
     message: 'Hubo un problema interno en el servidor.'
   })
 })
-
 
 app.listen(PORT, () => {
   logger.info(`Servidor corriendo en puerto ${PORT}`)
